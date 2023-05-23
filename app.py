e958639c6a40361fef0fb3c837558e3310120c1a
Amgad Hasan <109704569+AmgadHasan@users.noreply.github.com>
2023-05-23 20:48:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,28 +10,25 @@ text_preprocessor = TextPreprocessor()
 
 #Define predictions dictionary
 counrty_dict = {"EG": "مصري", "LY": "ليبي", "LB": "لباني" , "SD":"سوداني" ,"MA":"مغربي"}
+PAGE_ICON = ":wave:"
 
-# Define the Streamlit app
-def app():
-    # Create a text input field for the user to enter a string
-    input_string = st.text_input('أدخل نص عربي')
-    
-    # Create a button to make the prediction
-    if st.button('Make prediction'):
-        trans_data = text_preprocessor.transform([input_string])
-        # Check whether user inputted a valid string
-        if trans_data[0] == "" or trans_data[0] == " ":
-            text = "من فضلك أدخل جملة عربية صحيحة"
-        else:
-            # Make prediction using the model
-            prediction = model.predict(trans_data)
-            for i in counrty_dict:
-                if i == predict:
-                    dialect = counrty_dict[i]
+st.set_page_config(page_title="Dialect detector", page_icon=PAGE_ICON)
 
-        # Display the prediction as text
-        st.write(f'The model predicted: {dialect}')
+# Create a text input field for the user to enter a string
+input_string = st.text_input('أدخل نص عربي')
 
-# Start the Streamlit app
-if __name__ == '__main__':
-    app()
+# Create a button to make the prediction
+if st.button('Make prediction'):
+    trans_data = text_preprocessor.transform([input_string])
+    # Check whether user inputted a valid string
+    if trans_data[0] == "" or trans_data[0] == " ":
+        text = "من فضلك أدخل جملة عربية صحيحة"
+    else:
+        # Make prediction using the model
+        prediction = model.predict(trans_data)
+        for i in counrty_dict:
+            if i == predict:
+                dialect = counrty_dict[i]
+
+    # Display the prediction as text
+    st.write(f'The model predicted: {dialect}')

e6dcbef8a0eff3962914eac135f6d32b21d69e3d
Amgad Hasan <109704569+AmgadHasan@users.noreply.github.com>
2023-05-24 00:10:20 +0300

    Update api.py

--- code/api.py
+++ code/api.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 from preprocessing import TextPreprocessor
 from fastapi.templating import Jinja2Templates
 
-Declaring our FastAPI instance
+#Declaring our FastAPI instance
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
@@ -15,10 +15,10 @@ text_preprocessor = TextPreprocessor()
 country_dict = {"EG": "مصري", "LY": "ليبي", "LB": "لباني", "SD": "سوداني", "MA": "مغربي"}
 
 class PredictionResult(BaseModel):
-prediction: str
+    prediction: str
 
 class InputText(BaseModel):
-text: str
+    text: str
 
 CWD = os.getcwd()
 PWD = os.path.dirname(CWD)
@@ -28,16 +28,16 @@ model = joblib.load(SVM_PATH)
 
 @app.get("/")
 async def home(request: Request):
-return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/predict")
 async def predict(request: Request, text: str = Form(...)):
-text = text.strip()
-if not text:
-return templates.TemplateResponse("index.html", {"request": request, "error": "Please enter valid Arabic text."})
-else:
-test_data = [text]
-trans_data = text_preprocessor.transform(test_data)
-prediction = model.predict(trans_data)
-country = country_dict.get(prediction[0], "Unknown")
-return templates.TemplateResponse("index.html", {"request": request, "prediction": country})
+    text = text.strip()
+    if not text:
+        return templates.TemplateResponse("index.html", {"request": request, "error": "Please enter valid Arabic text."})
+    else:
+        test_data = [text]
+        trans_data = text_preprocessor.transform(test_data)
+        prediction = model.predict(trans_data)
+        country = country_dict.get(prediction[0], "Unknown")
+        return templates.TemplateResponse("index.html", {"request": request, "prediction": country})
